777a337d360db2aae0fc460dc5e3f424224163de
Ladya2000 <vladiatorone123@gmail.com>
2024-02-25 21:08:34 +0300

    add other stuff

---
+++ 3 course/1 sem/РПР/лаба 1/lab1/.local/install/CodedWorkflow.cs
@@ -0,0 +1,12 @@
+using UiPath.CodedWorkflows;
+
+namespace lab1
+{
+    public partial class CodedWorkflow : CodedWorkflowBase
+    {
+        public CodedWorkflow()
+        {
+            _ = new System.Type[]{};
+        }
+    }
+}
